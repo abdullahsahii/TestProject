45a6a372e77d5c168409ed69c4965934be21daa5
abdullahsahii <abdullah.sahi@southvillesol.com>
2023-08-02 12:52:01 +0500

    Apply cancancan for user permission

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,10 @@
 class Character < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
-    ["created", "created_at", "gender", "id", "image", "location_name", "location_url", "name", "origin_name", "origin_url", "species", "status", "type", "updated_at", "user_id"]
+    %w[created created_at gender id image location_name location_url name origin_name origin_url species status type updated_at user_id]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[comments user]
   end
   belongs_to :user
   has_many :comments
--- db/migrate/20230726100020_add_column_to_user.rb
+++ db/migrate/20230726100020_add_column_to_user.rb
@@ -1,5 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[6.1]
   def change
+    add_column :users, :role, :integer, default: 0
     add_column :users, :name, :string
     add_column :users, :age, :integer
   end
